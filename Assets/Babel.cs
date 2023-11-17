cd364ba58363c664f16065c0019e8b0701215f84
Gerald Krystian <amarcolina@gmail.com>
2023-11-16 23:48:11 -0800

    workin

--- Assets/Babel.cs
+++ Assets/Babel.cs
@@ -3,28 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Numerics;
 using Unity.Mathematics;
+using Unity.VisualScripting;
 
 [ExecuteAlways]
-public class Babel : MonoBehaviour
-{
+public class Babel : MonoBehaviour {
 
-    public int N = 16;
-    public int D = 0;
-    public int Pixels = 0;
+    public int ImageWidth = 16;
+    public int ImageSize = 0;
 
+    public bool CalculateCounts;
     public bool Increment;
 
     public int ImageDensity;
-    public int[] Image;
+    public int[] Patterns;
+    public int[] PatternCounts;
 
-    private void OnValidate()
-    {
-        N = Mathf.ClosestPowerOfTwo(N);
-        Pixels = N * N;
+    private void OnValidate() {
+        ImageWidth = Mathf.ClosestPowerOfTwo(ImageWidth);
+        ImageSize = ImageWidth * ImageWidth;
     }
 
-    private void OnEnable()
-    {
+    private void OnEnable() {
         //int levels = Mathf.RoundToInt(Mathf.Log(N) / Mathf.Log(2));
         //int curves = 0;
         //int curvesPerLevel = 1;
@@ -34,113 +33,114 @@ public class Babel : MonoBehaviour
         //    curvesPerLevel *= 4;
         //}
 
-        Image = new int[Pixels - 1];
+        Patterns = new int[ImageSize - 1];
+        PatternCounts = new int[ImageSize - 1];
     }
 
-    private void Update()
-    {
-        if (Increment)
-        {
+    private void Update() {
+        if (CalculateCounts) {
+            CalculateCounts = false;
+            CalculatePixelCounts();
+        }
+
+        if (Increment) {
             Increment = false;
             NextImage();
         }
     }
 
-    public void NextImage()
-    {
-        bool Increment(int index, int N)
-        {
-            if (N == 0)
-            {
-                return false;
+    public void CalculatePixelCounts() {
+        void Recurse(int level, int index, int allocated, int maxPerHalve) {
+            PatternCounts[index] = N2Count(allocated, maxPerHalve);
+            int2 allocation = N2XY(allocated, maxPerHalve, Patterns[index]);
+
+            if (index * 2 + 1 >= Patterns.Length) {
+                return;
             }
 
-            if (!IsLeaf(index))
-            {
-                int4 allocation = d2xyzw(N / 4, Image[index]);
-                if (Increment(index * 4 + 1, allocation.x)) return true;
-                if (Increment(index * 4 + 2, allocation.y)) return true;
-                if (Increment(index * 4 + 3, allocation.z)) return true;
-                if (Increment(index * 4 + 4, allocation.w)) return true;
-            }
+            Recurse(level + 1, index * 2 + 1, allocation.x, maxPerHalve / 2);
+            Recurse(level + 1, index * 2 + 2, allocation.y, maxPerHalve / 2);
+        }
 
-            if (Image[index] == (N - 1))
-            {
-                Image[index] = 0;
-                return false;
-            }
-            else
-            {
-                Image[index]++;
-                return true;
+        Recurse(0, 0, ImageDensity, ImageSize / 2);
+    }
+
+    public void NextImage() {
+        CalculatePixelCounts();
+
+        for (int i = Patterns.Length - 1; i >= 0; i--) {
+            Patterns[i]++;
+            if (Patterns[i] >= PatternCounts[i]) {
+                Patterns[i] = 0;
+                if (i == 0) {
+                    ImageDensity++;
+                }
+            } else {
+                break;
             }
         }
 
-        Increment(0, Pixels);
+        CalculatePixelCounts();
     }
 
-    public void Draw()
-    {
-        void DrawRecursively(int index, int N)
-        {
-            if (N == 0)
-            {
+    public void Draw() {
+        void DrawRecursively(int index, int N) {
+            if (N == 0) {
                 return;
             }
 
 
         }
 
-        DrawRecursively(0, Pixels);
+        DrawRecursively(0, ImageSize);
     }
 
 
-    public bool IsLeaf(int index)
-    {
-        return index * 4 + 1 >= Image.Length;
+    public bool IsLeaf(int index) {
+        return index * 4 + 1 >= Patterns.Length;
     }
 
-    [ContextMenu("Try")]
-    void OkTry()
-    {
-        var num = BigInteger.Pow(new BigInteger(2), 256 * 256);
-        Debug.Log(num.ToString());
+    private void OnDrawGizmos() {
     }
 
-    private void OnDrawGizmos()
-    {
-        int4 pos = d2xyzw(N, D);
 
-        Gizmos.DrawCube(new UnityEngine.Vector3(pos.x, pos.y, pos.z), UnityEngine.Vector3.one);
+    public int N2Count(int allocation, int maxAlloc) {
+        if (allocation < maxAlloc) maxAlloc = allocation;
 
-        //Gizmos.color = new Color(a, a, a, 1);
-        //Gizmos.DrawCube(new Vector3(0, 0, 0), Vector3.one);
-        //Gizmos.color = new Color(b, b, b, 1);
-        //Gizmos.DrawCube(new Vector3(1, 0, 0), Vector3.one);
+        int minAlloc = allocation - maxAlloc;
+        int validCount = maxAlloc - minAlloc + 1;
 
-        for (int x = 0; x < N; x++)
-        {
-            for (int y = 0; y < N; y++)
-            {
-                for (int z = 0; z < N; z++)
-                {
-                    if (x + y + z == N)
-                    {
-                        Gizmos.DrawCube(new UnityEngine.Vector3(x, y, z), new UnityEngine.Vector3(1, 1, 1));
-                    }
-                }
-            }
+        return validCount;
+    }
+
+    public int2 N2XY(int count, int maxAlloc, int index) {
+        if (count < maxAlloc) maxAlloc = count;
+
+        int minAlloc = count - maxAlloc;
+        int validCount = maxAlloc - minAlloc + 1;
+
+        if (validCount <= 0) {
+            return 0;
         }
+
+        int even = (validCount + 1) / 2;
+        int odd = validCount - even;
+
+        int x;
+        if (index < even) {
+            x = minAlloc + index * 2;
+        } else {
+            x = minAlloc + even * 2 - 1 - (index - even) * 2;
+            if (validCount % 2 == 1) x -= 2;
+        }
+
+        return new int2(x, count - x);
     }
-
-
 
     //convert (x,y) to d
-    int xy2d(int n, int x, int y)
-    {
+    int xy2d(int n, int x, int y) {
         int rx, ry, s, d = 0;
-        for (s = n / 2; s > 0; s /= 2)
-        {
+        for (s = n / 2; s > 0; s /= 2) {
             rx = (x & s) > 0 ? 1 : 0;
             ry = (y & s) > 0 ? 1 : 0;
             d += s * s * ((3 * rx) ^ ry);
@@ -150,12 +150,10 @@ public class Babel : MonoBehaviour
     }
 
     //convert d to (x,y)
-    void d2xy(int n, int d, ref int x, ref int y)
-    {
+    void d2xy(int n, int d, ref int x, ref int y) {
         int rx, ry, s, t = d;
         x = y = 0;
-        for (s = 1; s < n; s *= 2)
-        {
+        for (s = 1; s < n; s *= 2) {
             rx = 1 & (t / 2);
             ry = 1 & (t ^ rx);
             rot(s, ref x, ref y, rx, ry);
@@ -165,8 +163,7 @@ public class Babel : MonoBehaviour
         }
     }
 
-    int4 d2xyzw(int n, int d)
-    {
+    int4 d2xyzw(int n, int d) {
         int x = d % n;
         if (d / n % 2 == 1) x = (n - 1) - x;
 
@@ -182,12 +179,9 @@ public class Babel : MonoBehaviour
     }
 
     //rotate/flip a quadrant appropriately
-    void rot(int n, ref int x, ref int y, int rx, int ry)
-    {
-        if (ry == 0)
-        {
-            if (rx == 1)
-            {
+    void rot(int n, ref int x, ref int y, int rx, int ry) {
+        if (ry == 0) {
+            if (rx == 1) {
                 x = n - 1 - x;
                 y = n - 1 - y;
             }
