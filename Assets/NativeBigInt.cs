7277bb82eee9f584c9ae030b9b6dc400bb48893a
Alex Marcolina <amarcolina@gmail.com>
2025-03-17 15:56:40 -0700

    I wonder if it can be faster

--- Assets/NativeBigInt.cs
+++ Assets/NativeBigInt.cs
@@ -1,13 +1,52 @@
+using System.Numerics;
 using Unity.Collections;
-using Unity.Mathematics;
 using UnityEngine;
-using Unity.Burst.Intrinsics;
 
 public static class NativeBigUInt {
 
-  public static void Add(NativeArray<ulong> target, int value) {
+  public static void Init(NativeArray<ulong> target, BigInteger value) {
+    for (int i = 0; i < target.Length; i++) {
+      ulong digit = (ulong)(value & ulong.MaxValue);
+      target[i] = digit;
+      value = value >> 64;
+    }
+  }
+
+  public static void Init(NativeArray<ulong> target, ulong value) {
+    target[0] = value;
+    for (int i = 1; i < target.Length; i++) {
+      target[i] = 0;
+    }
+  }
+
+  public static bool Equals(NativeArray<ulong> target, ulong value) {
+    for (int i = 1; i < target.Length; i++) {
+      if (target[i] != 0) {
+        return false;
+      }
+    }
+    return target[0] == value;
+  }
+
+  public static bool Equals(NativeArray<ulong> target, BigInteger value) {
+    if (value < 0) {
+      return false;
+    }
+
+    for (int i = 0; i < target.Length; i++) {
+      ulong digit = (ulong)(value & ulong.MaxValue);
+      if (digit != target[i]) {
+        return false;
+      }
+      value = value >> 64;
+    }
+
+    return value == 0;
+  }
+
+  public static void Add(NativeArray<ulong> target, ulong value) {
     var prevDigit = target[0];
-    target[0] += (ulong)value;
+    target[0] += value;
     if (target[0] < prevDigit && target.Length > 1) {
       target[1]++;
     }
@@ -32,16 +71,44 @@ public static class NativeBigUInt {
     }
   }
 
-  public static void Sub(NativeArray<ulong> target, int value) {
+  public static void Sub(NativeArray<ulong> target, ulong value) {
+    if (target[0] < value) {
+      int i = 1;
+      while (i < target.Length) {
+        if (target[i] != 0) {
+          target[i]--;
+          break;
+        } else {
+          target[i]--;
+        }
+      }
+    }
 
+    target[0] -= value;
   }
 
   public static void Sub(NativeArray<ulong> target, NativeArray<ulong> arg) {
+    int minIndex = Mathf.Min(target.Length, arg.Length);
 
+    for (int i = 0; i < minIndex; i++) {
+      if (target[i] < arg[i]) {
+        int j = i + 1;
+        while (j < target.Length) {
+          if (target[j] != 0) {
+            target[j]--;
+            break;
+          } else {
+            target[j]--;
+          }
+        }
+      }
+
+      target[i] -= arg[i];
+    }
   }
 
   public static void Mul(NativeArray<ulong> target, int value) {
-    
+
   }
 
 }
